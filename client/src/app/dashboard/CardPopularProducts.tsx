114c56e2f310a08ab48eeca4100f04241b589362
neopinp <Neo.Pi1@marist.edu>
2024-08-11 01:06:55 -0400

    Dashboard components added

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -1,8 +1,8 @@
-import { useGetDashboardMetricsQuery } from "@/state/api";
-import { ShoppingBag } from "lucide-react";
-import React from "react";
-import Rating from "../(components)/Rating";
-import Image from "next/image";
+import { useGetDashboardMetricsQuery } from '@/state/api';
+import { ShoppingBag } from 'lucide-react';
+import React from 'react';
+import Rating from '../(components)/Rating';
+import Image from 'next/image';
 
 const CardPopularProducts = () => {
   const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
@@ -24,7 +24,7 @@ const CardPopularProducts = () => {
                 className="flex items-center justify-between gap-3 px-5 py-7 border-b"
               >
                 <div className="flex items-center gap-3">
-                    <div>img</div>
+                  <div>img</div>
                   <div className="flex flex-col justify-between gap-1">
                     <div className="font-bold text-gray-700">
                       {product.name}
