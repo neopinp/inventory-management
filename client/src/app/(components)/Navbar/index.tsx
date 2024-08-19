769d0410b797d5bf2d25983f8fa7b1cb2c65321a
neopinp <123033639+neopinp@users.noreply.github.com>
2024-08-19 00:07:36 -0400

    profile

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -66,7 +66,7 @@ const Navbar = () => {
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
             <Image
-              src="https://s3-inventory-management-pi.s3.us-east-2.amazonaws.com/profile.png"
+              src="https://s3-inventory-management-pi.s3.us-east-2.amazonaws.com/profile.jpg"
               alt="Profile"
               width={50}
               height={50}
